fc85a19ce757d7f8219fd8e25a81e458497730b8
vloss <viniciusvianaloss@gmail.com>
2023-02-21 18:22:15 -0300

    Iniciando projeto.

---
+++ Fotoroid/Fotoroid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fotoroid
+//
+//  Created by Vinicius Loss on 21/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
