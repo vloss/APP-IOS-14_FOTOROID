de4e896efc777209e83ec0420f049e7936c96deb
vloss <viniciusvianaloss@gmail.com>
2023-02-21 23:44:50 -0300

    Configuração de elementos visuais. Criação de viewController para Efeitos e tela final.

---
+++ Fotoroid/Fotoroid/EffectsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EffectsViewController.swift
+//  Fotoroid
+//
+//  Created by Vinicius Loss on 21/02/23.
+//
+
+import UIKit
+
+class EffectsViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var viLoading: UIView!
+    @IBOutlet weak var viPhoto: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Fotoroid/Fotoroid/FinalViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FinalViewController.swift
+//  Fotoroid
+//
+//  Created by Vinicius Loss on 21/02/23.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+
+    @IBOutlet weak var ivPhoto: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func restart(_ sender: UIButton) {
+        
+    }
+}
