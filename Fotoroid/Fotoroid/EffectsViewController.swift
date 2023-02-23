61bdcba0593e9367dbce3e1c1756315bf6cba4a6
vloss <viniciusvianaloss@gmail.com>
2023-02-22 22:25:06 -0300

    Criado o metodo para aplicação de filtro em uma imagem.

--- Fotoroid/Fotoroid/EffectsViewController.swift
+++ Fotoroid/Fotoroid/EffectsViewController.swift
@@ -15,10 +15,20 @@ class EffectsViewController: UIViewController {
     
     
     var image: UIImage!
+    lazy var filterManager: FilterManager = {
+        let filterManager = FilterManager(image: image)
+        return filterManager
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ivPhoto.image = image
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        ivPhoto.image = filterManager.applyFilter(type: FilterType(rawValue: 5)!)
     }
     
     // Vai aparecer
--- Fotoroid/Fotoroid/FilterManager.swift
+++ Fotoroid/Fotoroid/FilterManager.swift
@@ -34,6 +34,33 @@ class FilterManager {
     }
     
     func applyFilter (type: FilterType) -> UIImage{
-        
+
+        let ciImage  = CIImage(image: originalImage)!
+        let filter = CIFilter(name: filtersNames[type.rawValue])!
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
+
+        switch type {
+            case .comic:
+                break
+            case .sepia:
+                filter.setValue(1.0, forKey: kCIInputIntensityKey)
+            case .halftone:
+                filter.setValue(25, forKey: kCIInputWidthKey)
+            case .crystallize:
+                filter.setValue(25, forKey: kCIInputRadiusKey)
+            case .vignette:
+                filter.setValue(3, forKey: kCIInputIntensityKey)
+                filter.setValue(30, forKey: kCIInputRadiusKey)
+            case .noir:
+                break
+            default:
+                break
+        }
+
+        let filteredImage = filter.value(forKey: kCIOutputImageKey) as! CIImage
+
+        let cgImage = context.createCGImage(filteredImage, from: filteredImage.extent)
+
+       return UIImage(cgImage: cgImage!)
     }
 }
